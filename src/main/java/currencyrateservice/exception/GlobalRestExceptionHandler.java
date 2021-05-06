411d5773a632bc752589ee37f13f30c1f3dc90ab
AlexKlim <klymenko.oleksandr.2017@gmail.com>
2021-05-06 16:56:34 +0300

    Corrected GlobalRestExceptionHandler

--- src/main/java/currencyrateservice/exception/GlobalRestExceptionHandler.java
+++ src/main/java/currencyrateservice/exception/GlobalRestExceptionHandler.java
@@ -4,6 +4,7 @@ import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -25,14 +26,20 @@ public class GlobalRestExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 
+    @ExceptionHandler(JsonParsingException.class)
+    public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException exception) {
+        log.debug(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
+    }
+
     @ExceptionHandler(ExternalDataSourceException.class)
-    public ResponseEntity<Object> handleInvalidInputException(ExternalDataSourceException exception) {
+    public ResponseEntity<Object> handleExternalDataSourceException(ExternalDataSourceException exception) {
         log.debug(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 
     @ExceptionHandler(JsonParsingException.class)
-    public ResponseEntity<Object> handleInvalidInputException(JsonParsingException exception) {
+    public ResponseEntity<Object> handleJsonParsingException(JsonParsingException exception) {
         log.debug(exception.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
